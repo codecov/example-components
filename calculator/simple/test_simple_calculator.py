ffcd2752cee8347d56e1f47cee6025ae70443d53
Tom Hu <tom@codecov.io>
2023-01-28 16:19:39 -0500

    show Components in the Codecov PR comment

--- calculator/simple/test_simple_calculator.py
+++ calculator/simple/test_simple_calculator.py
@@ -3,3 +3,6 @@ from .simple_calculator import SimpleCalculator
 
 def test_add():
     assert SimpleCalculator.add(1, 2) == 3.0
+
+def test_subtract():
+    assert SimpleCalculator.subtract(1, 2) == -1.0
